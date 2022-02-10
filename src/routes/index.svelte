45dbb2f50c4169f93d91d1b7d38ddc3f88a552ed
Romain l'Ourson <rcrestey@gmail.com>
2022-02-10 17:50:41 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,7 +16,7 @@
     <p>
       Une écoute et un parcours <strong>adaptés à chacun</strong>, pour vous
       accompagner dans l'atteinte de <strong>vos objectifs</strong> personnels et
-      professionnels.
+      professionnels. COUCOU
     </p>
   </div>
 </section>
