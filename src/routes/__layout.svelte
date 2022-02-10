d49767f9026215a46c9b69e3a3dbade4cb233696
Romain Crestey <rcrestey@gmail.com>
2022-02-10 09:24:17 +0100

    refactor: simplify global styles syntax

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -33,82 +33,84 @@
     // padding: 0rem 1rem
     padding: 0
 
-    :global(h1)
-      margin: 1.5em 0
-      font-size: 1.5em
-      text-align: center
+    :global
 
-    :global(a)
-      color: var(--pink)
-      text-decoration: none
-    :global(a:hover), :global(a:focus)
-      outline: none
-      text-decoration: underline
+      h1
+        margin: 1.5em 0
+        font-size: 1.5em
+        text-align: center
 
-    :global(p)
-      margin: 1.5rem 0
-      line-height: 1.4em
-    :global(p:last-child)
-      margin-bottom: 3rem
-
-    :global(section)
-      display: flex
-      flex-flow: column
-      position: relative
-      margin: 2em 0
-
-    :global(section .img-container)
-      width: 100%
-
-    :global(section img)
-      filter: opacity(70%)
-
-    :global(.content)
-      padding: 0 1.5rem
-
-    :global(a.button)
-      margin: 0 auto
-      padding: 0.7em 1.5em
-      font-size: 1.2em
-      color: white
-      background: var(--pink)
-      border-radius: 0.5em
-
-      &:hover, &:focus
-        box-shadow: 0px 0px 5px 0px rgba(black, 0.5)
+      a
+        color: var(--pink)
         text-decoration: none
+      a:hover, a:focus
+        outline: none
+        text-decoration: underline
 
-    :global(h2)
-      position: sticky
-      top: 4.5rem
-      margin: 1.5rem 0
-      margin-bottom: 0
-      background: white
+      p
+        margin: 1.5rem 0
+        line-height: 1.4em
+      p:last-child
+        margin-bottom: 3rem
+
+      section
+        display: flex
+        flex-flow: column
+        position: relative
+        margin: 2em 0
+
+      section .img-container
+        width: 100%
+
+      section img
+        filter: opacity(70%)
+
+      .content
+        padding: 0 1.5rem
+
+      a.button
+        margin: 0 auto
+        padding: 0.7em 1.5em
+        font-size: 1.2em
+        color: white
+        background: var(--pink)
+        border-radius: 0.5em
+
+        &:hover, &:focus
+          box-shadow: 0px 0px 5px 0px rgba(black, 0.5)
+          text-decoration: none
+
+      h2
+        position: sticky
+        top: 4.5rem
+        margin: 1.5rem 0
+        margin-bottom: 0
+        background: white
+        width: 100%
+        z-index: 1
+        font-size: 1.2em
+        font-weight: bold
+        padding: 0.5em
+        padding-left: 1em
+        background: var(--blue)
+        color: white
+        box-shadow: 0px 0px 5px 0px rgba(black, 0.5)
+
+      ul
+        padding-left: 1.5em
+        list-style: disc
+
+      li
+        margin: 0.5em 0
+
+  :global
+    .img-container
+      display: flex
+      overflow: hidden
+      margin: auto
       width: 100%
-      z-index: 1
-      font-size: 1.2em
-      font-weight: bold
-      padding: 0.5em
-      padding-left: 1em
-      background: var(--blue)
-      color: white
-      box-shadow: 0px 0px 5px 0px rgba(black, 0.5)
 
-    :global(ul)
-      padding-left: 1.5em
-      list-style: disc
-
-    :global(li)
-      margin: 0.5em 0
-
-  :global(.img-container)
-    display: flex
-    overflow: hidden
-    margin: auto
-    width: 100%
-    max-height: 15rem
-
-    :global(img)
+    img
       object-fit: contain
       width: 100%
       height: 100%
@@ -121,42 +123,45 @@
       &.home
         padding: 2rem 4rem
 
-      :global(h1)
-        font-size: 2em
-        margin-top: 1em
-        margin-bottom: 2em
+      :global
+        h1
+          font-size: 2em
+          margin-top: 1em
+          margin-bottom: 2em
 
-      :global(h2)
-        position: unset
-        padding-left: 0.5em
-        margin-bottom: 0.5em
-        width: 70%
-        text-align: center
+        h2
+          position: unset
+          padding-left: 0.5em
+          margin-bottom: 0.5em
+          width: 70%
+          text-align: center
 
-      :global(section)
-        margin-top: 2rem
-        display: flex
-        flex-flow: column
+        section
+          margin-top: 2rem
+          display: flex
+          flex-flow: column
 
-      :global(section .img-container)
-        position: absolute
-        top: 0
-        width: 35%
-        right: 0
-        border-radius: 8px
-        overflow: hidden
+        section .img-container
+          position: absolute
+          top: 0
+          width: 35%
+          right: 0
+          border-radius: 8px
+          overflow: hidden
 
-      :global(.content)
-        padding: 0
-        width: 60%
-        margin-left: 1rem
+        .content
+          padding: 0
+          width: 60%
+          margin-left: 1rem
 
-    :global(.img-container)
-      max-height: unset
+    :global
+      .img-container
+        max-height: unset
 
   @include desktop
-    :global(p), :global(li)
-      font-size: 1.1rem
+    :global
+      p, li
+        font-size: 1.1rem
 
     main
       width: 70%
